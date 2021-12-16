844817d43c470ba0c7d8a06c6571144148383eef
nguyenvd27 <ducnguyen272727@gmail.com>
2021-12-16 14:23:12 +0700

    empty branch

--- graph/model/todo.go
+++
@@ -1,8 +0,0 @@
-package graphqlmodel
-
-type Todo struct {
-	ID     string `json:"id"`
-	Text   string `json:"text"`
-	Done   bool   `json:"done"`
-	UserID string `json:"user"`
-}
--- model/event.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Event struct {
-	ID      string `json:"id"`
-	Title   string `json:"title"`
-	Content string `json:"content"`
-	UserID  string `json:"user_id"`
-}
